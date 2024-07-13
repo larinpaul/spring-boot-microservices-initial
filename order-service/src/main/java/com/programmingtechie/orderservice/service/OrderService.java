6eeb0fd24205429a08d259bff772b90c4008aed9
larinpaul <www.larinpavel@mail.ru>
2024-07-13 11:41:31 +0500

    Re-creaated the placeOrder method

--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -26,12 +26,7 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
 
-    private final OrderRepository orderRepository;
-    private final WebClient.Builder webClientBuilder;
-    private final ObservationRegistry observationRegistry;
-    private final ApplicationEventPublisher applicationEventPublisher;
-
-    public String placeOrder(OrderRequest orderRequest) {
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -42,43 +37,76 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        List<String> skuCodes = order.getOrderLineItemsList().stream()
-                .map(OrderLineItems::getSkuCode)
-                .toList();
-
         // Call Inventory Service, and place order if product is in
         // stock
-        Observation inventoryServiceObservation = Observation.createNotStarted("inventory-service-lookup",
-                this.observationRegistry);
-        inventoryServiceObservation.lowCardinalityKeyValue("call", "inventory-service");
-        return inventoryServiceObservation.observe(() -> {
-            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                    .uri("http://inventory-service/api/inventory",
-                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                    .retrieve()
-                    .bodyToMono(InventoryResponse[].class)
-                    .block();
-
-            boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
-                    .allMatch(InventoryResponse::isInStock);
-
-            if (allProductsInStock) {
-                orderRepository.save(order);
-                // publish Order Placed Event
-                applicationEventPublisher.publishEvent(new OrderPlacedEvent(this, order.getOrderNumber()));
-                return "Order Placed";
-            } else {
-                throw new IllegalArgumentException("Product is not in stock, please try again later");
-            }
-        });
-
+        orderRepository.save(order);
     }
 
-    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+
+
+
+     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
         orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
         return orderLineItems;
     }
+
+
+//    private final OrderRepository orderRepository;
+//    private final WebClient.Builder webClientBuilder;
+//    private final ObservationRegistry observationRegistry;
+//    private final ApplicationEventPublisher applicationEventPublisher;
+//
+//    public String placeOrder(OrderRequest orderRequest) {
+//        Order order = new Order();
+//        order.setOrderNumber(UUID.randomUUID().toString());
+//
+//        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+//                .stream()
+//                .map(this::mapToDto)
+//                .toList();
+//
+//        order.setOrderLineItemsList(orderLineItems);
+//
+//        List<String> skuCodes = order.getOrderLineItemsList().stream()
+//                .map(OrderLineItems::getSkuCode)
+//                .toList();
+//
+//        // Call Inventory Service, and place order if product is in
+//        // stock
+//        Observation inventoryServiceObservation = Observation.createNotStarted("inventory-service-lookup",
+//                this.observationRegistry);
+//        inventoryServiceObservation.lowCardinalityKeyValue("call", "inventory-service");
+//        return inventoryServiceObservation.observe(() -> {
+//            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+//                    .uri("http://inventory-service/api/inventory",
+//                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+//                    .retrieve()
+//                    .bodyToMono(InventoryResponse[].class)
+//                    .block();
+//
+//            boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+//                    .allMatch(InventoryResponse::isInStock);
+//
+//            if (allProductsInStock) {
+//                orderRepository.save(order);
+//                // publish Order Placed Event
+//                applicationEventPublisher.publishEvent(new OrderPlacedEvent(this, order.getOrderNumber()));
+//                return "Order Placed";
+//            } else {
+//                throw new IllegalArgumentException("Product is not in stock, please try again later");
+//            }
+//        });
+//
+//    }
+//
+//    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+//        OrderLineItems orderLineItems = new OrderLineItems();
+//        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+//        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+//        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+//        return orderLineItems;
+//    }
 }
