fe307f14945c51d44ecaa22ff6f2335a4cbcd381
larinpaul <www.larinpavel@mail.ru>
2024-07-15 08:00:02 +0500

    Created @Configuration WebClientConfig.java

--- order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.programmingtechie.orderservice.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,4 +14,5 @@ public class WebClientConfig {
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
+
 }
