adfd73a92bed7da7d9d249649b635b1758d5b852
larinpaul <www.larinpavel@mail.ru>
2024-07-26 07:45:38 +0500

    Wrote InventoryResponse.java package com.programmingtechie.orderservice.dto;
    import lombok.AllArgstConstructor;
    import lombok.Builder;
    import lombok.Data;
    import lombok.NoArgsConstructor;
    
    @Data
    @AllArgsConstructor
    @NoArgsConstructor
    @Builder
    public class InventoryResponse {
        private String skuCode;
        private boolean isInStock;
    }

--- order-service/src/main/java/com/programmingtechie/orderservice/dto/InventoryResponse.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/dto/InventoryResponse.java
@@ -13,3 +13,19 @@ public class InventoryResponse {
     private String skuCode;
     private boolean isInStock;
 }
+
+//package com.programmingtechie.orderservice.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class InventoryResponse {
+//    private String skuCode;
+//    private boolean isInStock;
+//}
